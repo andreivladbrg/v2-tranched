6578dd76d4e04c6d769134b5d0b8296ba26853df
andreivladbrg <andreivladbrg@gmail.com>
2024-01-15 13:08:03 +0200

    test: remove one function for dynamic benchmarks and declare an array of size 13

--- test/Benchmarks.t.sol
+++ test/Benchmarks.t.sol
@@ -18,26 +18,7 @@ contract Benchmarks is Base_Test {
     // multiple zero slots to non-zero values. This is we start from an index of 1.
 
     function test_CreateWithMilestones_LockupDynamic_GasTests() external {
-        uint8[9] memory segmentCounts = [1, 4, 8, 12, 24, 48, 72, 96, 120];
-        uint256[] memory beforeGas = new uint256[](segmentCounts.length);
-        uint256[] memory afterGas = new uint256[](segmentCounts.length);
-
-        LockupDynamic.CreateWithMilestones memory params;
-        for (uint256 i = 0; i < segmentCounts.length; ++i) {
-            params = getDynamicParams(segmentCounts[i]);
-            beforeGas[i] = gasleft();
-            lockupDynamic.createWithMilestones(params);
-            afterGas[i] = gasleft();
-        }
-
-        for (uint256 i = 1; i < segmentCounts.length; ++i) {
-            uint256 gasUsed = beforeGas[i] - afterGas[i];
-            console2.log("Gas used for createWithMilestones: ", gasUsed, " with segments length: ", segmentCounts[i]);
-        }
-    }
-
-    function test_CreateWithMilestones2_LockupDynamic_GasTests() external {
-        uint8[9] memory segmentCounts = [1, 2, 4, 6, 12, 24, 36, 48, 60];
+        uint8[13] memory segmentCounts = [1, 2, 4, 6, 8, 12, 24, 36, 48, 60, 72, 96, 120];
         uint256[] memory beforeGas = new uint256[](segmentCounts.length);
         uint256[] memory afterGas = new uint256[](segmentCounts.length);
 
@@ -79,41 +60,7 @@ contract Benchmarks is Base_Test {
     //////////////////////////////////////////////////////////////////////////*/
 
     function test_Withdraw_LockupDynamic_GasTests() external {
-        uint8[9] memory segmentCounts = [4, 4, 8, 12, 24, 48, 72, 96, 120];
-
-        uint256[] memory streamIds = new uint256[](segmentCounts.length);
-
-        LockupDynamic.CreateWithMilestones memory params;
-        for (uint256 i = 0; i < segmentCounts.length; ++i) {
-            params = getDynamicParams(segmentCounts[i]);
-            streamIds[i] = lockupDynamic.createWithMilestones(params);
-        }
-
-        uint256[] memory beforeGas = new uint256[](segmentCounts.length);
-        uint256[] memory afterGas = new uint256[](segmentCounts.length);
-
-        uint40 warpTime;
-        uint128 withdrawAmount;
-        for (uint256 i = 0; i < segmentCounts.length; ++i) {
-            // 10 seconds before the end time to calculate the streamed amount for the last segment
-            warpTime = lockupDynamic.getEndTime(streamIds[i]) - 10;
-            vm.warp(warpTime);
-
-            withdrawAmount = lockupDynamic.withdrawableAmountOf(streamIds[i]);
-
-            beforeGas[i] = gasleft();
-            lockupDynamic.withdraw(streamIds[i], recipient, withdrawAmount);
-            afterGas[i] = gasleft();
-        }
-
-        for (uint256 i = 1; i < segmentCounts.length; ++i) {
-            uint256 gasUsed = beforeGas[i] - afterGas[i];
-            console2.log("Gas used for withdraw: ", gasUsed, " with segments length: ", segmentCounts[i]);
-        }
-    }
-
-    function test_Withdraw2_LockupDynamic_GasTests() external {
-        uint8[9] memory segmentCounts = [2, 2, 4, 6, 12, 24, 36, 48, 60];
+        uint8[13] memory segmentCounts = [1, 2, 4, 6, 8, 12, 24, 36, 48, 60, 72, 96, 120];
 
         uint256[] memory streamIds = new uint256[](segmentCounts.length);
 
